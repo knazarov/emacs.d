4037a636292b8ad548fa5a8c0c6a783d08eb3af9
Konstantin Nazarov <knazarov@Konstantins-iMac.local>
2020-09-12 02:09:42 +0300

    Update Lua mode to support emacs 27

--- third-party/third-party-autoloads.el
+++ third-party/third-party-autoloads.el
@@ -1283,6 +1283,24 @@ When called interactively, switch to the process buffer.
 
 \(fn &optional NAME PROGRAM STARTFILE &rest SWITCHES)" t nil)
 
+(autoload 'lua-mode "lua-mode" "\
+Major mode for editing Lua code.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
+
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(defalias 'run-lua #'lua-start-process)
+
+(autoload 'lua-start-process "lua-mode" "\
+Start a Lua process named NAME, running PROGRAM.
+PROGRAM defaults to NAME, which defaults to `lua-default-application'.
+When called interactively, switch to the process buffer.
+
+\(fn &optional NAME PROGRAM STARTFILE &rest SWITCHES)" t nil)
+
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lua-mode" '("lua-")))
 
 ;;;***
